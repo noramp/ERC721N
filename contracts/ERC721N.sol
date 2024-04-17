6f0e3beebdd1ddaab5f65963272e5a380c71bb5b
rarepepi <pepimartinez@protonmail.com>
2024-04-17 16:07:03 -0400

    clean

--- contracts/ERC721N.sol
+++ contracts/ERC721N.sol
@@ -7,7 +7,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-// SafeERC20
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /**
